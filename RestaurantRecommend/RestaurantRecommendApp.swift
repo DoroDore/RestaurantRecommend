a1118130a27cb34a02db760462fb7e900bd06b45
DoroDore <tlyz82@student.cis.eduhk>
2025-06-22 12:42:44 -0700

    Initial Commit

---
+++ RestaurantRecommend/RestaurantRecommendApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantRecommendApp.swift
+//  RestaurantRecommend
+//
+//  Created by Theodore Zhu on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantRecommendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestaurantRecommendTests/RestaurantRecommendTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantRecommendTests.swift
+//  RestaurantRecommendTests
+//
+//  Created by Theodore Zhu on 6/22/25.
+//
+
+import Testing
+@testable import RestaurantRecommend
+
+struct RestaurantRecommendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
